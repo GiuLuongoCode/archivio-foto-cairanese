c950defc2a1576654223e21d2c0270510fc0132c
GiuLuongoCode <giuseppe.luongo.dev@gmail.com>
2024-05-12 00:20:18 +0200

    android complete

--- src/app/photo/page.tsx
+++ src/app/photo/page.tsx
@@ -18,24 +18,9 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import {
-  Cloud,
-  CreditCard,
-  Github,
-  Keyboard,
-  LifeBuoy,
-  LogOut,
-  Mail,
-  MessageSquare,
-  Plus,
-  PlusCircle,
-  Settings,
-  User,
-  UserPlus,
-  Users,
-} from "lucide-react";
 import { Button } from "@/components/ui/button";
-
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 const greatVibes = Great_Vibes({
   subsets: ["latin"],
   weight: "400",
@@ -43,50 +28,56 @@ const greatVibes = Great_Vibes({
 });
 
 export default function Photo() {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  
   return (
     <>
       <Navbar />
       <SearchBar></SearchBar>
       <p
-        className={`${greatVibes.className} text-5xl border-b-2 border-dashed my-10`}
+        className={`${greatVibes.className} text-3xl md:text-4xl lg:text-5xl border-b-2 border-dashed my-10`}
       >
         Ricerca
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="col-span-full md:col-span-1 bg-gray-200 p-4">
-          <p>Risultati</p>
-        </div>
-        <div className="col-span-full md:col-span-1 bg-gray-300 p-4 flex justify-end">
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button variant="outline">
-                Ordina per
-                <svg
-                  className="w-2.5 h-2.5 ms-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 1 4 4 4-4"
-                  />
-                </svg>
+      <div className="flex flex-row justify-between">
+        <p>Risultati</p>
+        <div className="flex flex-row">
+          {isMobile ? (
+            <>
+              <Button>
+                <img src="/filtro.png" alt="filtro" />
               </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <img src="/burger.png" alt="burger" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Billing</DropdownMenuItem>
+                  <DropdownMenuItem>Team</DropdownMenuItem>
+                  <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  Ordina per
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Billing</DropdownMenuItem>
+                  <DropdownMenuItem>Team</DropdownMenuItem>
+                  <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          )}
         </div>
       </div>
       <Gallery></Gallery>
