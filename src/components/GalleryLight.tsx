d1cd5b4ca5d1b32c6d58391d500027a45e02448a
GiuLuongoCode <giuseppe.luongo.dev@gmail.com>
2024-06-05 18:51:52 +0200

    remove unused code

--- src/components/GalleryLight.tsx
+++ src/components/GalleryLight.tsx
@@ -16,35 +16,10 @@ import lgAutoplay from "lightgallery/plugins/autoplay";
 import lgFullscreen from "lightgallery/plugins/fullscreen";
 import lgShare from "lightgallery/plugins/share";
 import lgRotate from "lightgallery/plugins/rotate";
-// Infinite scroll
-// import { useInView } from "react-intersection-observer";
 import { Key } from "react";
 import { gql } from "graphql-tag";
 import { getClient } from "@/graphql/apolloClient";
 
-const initImages = [
-  { src: "/1.jpg", alt: "Flag of India" },
-  { src: "/2.jpg", alt: "2" },
-  { src: "/3.jpg", alt: "3" },
-  { src: "/4.jpg", alt: "4" },
-  { src: "/5.jpg", alt: "5" },
-  { src: "/6.jpg", alt: "6" },
-  { src: "/7.jpg", alt: "7" },
-  { src: "/8.jpg", alt: "8" },
-  { src: "/9.jpg", alt: "9" },
-];
-const secondImages = [
-  { src: "/10.jpg", alt: "10" },
-  { src: "/11.jpg", alt: "11" },
-  { src: "/12.jpg", alt: "12" },
-  { src: "/13.jpg", alt: "13" },
-  { src: "/14.jpg", alt: "14" },
-  { src: "/15.jpg", alt: "15" },
-  { src: "/16.jpg", alt: "16" },
-  { src: "/17.jpg", alt: "17" },
-  { src: "/18.jpg", alt: "18" },
-  { src: "/19.jpg", alt: "19" },
-];
 
 const SEARCH_IMAGES = gql`
   query SearchImages($query: String, $tags: [String]) {
@@ -58,9 +33,6 @@ const SEARCH_IMAGES = gql`
 `;
 
 export async function Gallery() {
-  // infinte scroll
-  //   const { ref, inView } = useInView();
-
   const client = getClient();
   const {data} = await client.query({
     query: SEARCH_IMAGES,
@@ -68,23 +40,9 @@ export async function Gallery() {
 
   console.log(data.searchImages);
 
-  // const {loading, error, data} = useQuery(SEARCH_IMAGES, {
-  //   variables: { query: "Giuseppe" },
-  // });
   const onInit = () => {
     console.log("lightGallery has been initialized");
   };
-
-  // Infinite scroll
-  //   useEffect(() => {
-  //     if (inView) {
-  //       setImages([...images, ...secondImages]);
-  //     }
-  //   }, [inView]);
-
-  // const loadMoreImages = () => {
-  //   setImages([...images, ...secondImages]);
-  // };
 
   return (
     <div className="grid grid-cols-1 gap-3">
